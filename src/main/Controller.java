772d63a59354afb42f77b4d9df780b3743f412f2
Romeo Bellon <romeo.bellon@me.com>
2015-12-02 12:10:42 -0500

    Added jNetPcap (libpcap is a prerequisite). In Linux check /usr/lib for libjnetpcap.so and libpcap.so.X.X.X! Please use IntelliJ

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -1,8 +1,36 @@
 package main;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import org.jnetpcap.Pcap;
+import org.jnetpcap.PcapIf;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Controller {
-    @FXML TextField ip;
+    List<PcapIf> alldevs = new ArrayList<>(); // Will be filled with NICs
+    StringBuilder errbuf = new StringBuilder(); // For any error msgs
+    ObservableList<String> devices = FXCollections.observableArrayList();
+
+    @FXML
+    TextField textfield_ip;
+    @FXML
+    ListView<String> list_devices = new ListView<>(devices);
+
+    public void getDevices() {
+        int r = Pcap.findAllDevs(alldevs, errbuf);
+        if (r == Pcap.NOT_OK || alldevs.isEmpty()) {
+            System.err.printf("Can't read list of devices, error is %s", errbuf.toString());
+        } else {
+            System.out.println(r);
+            devices.removeAll();
+            for (PcapIf alldev : alldevs) {
+                devices.add(alldev.getName());
+            }
+        }
+    }
 }
