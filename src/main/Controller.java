d5aabbac2786c6a3bad2cd48b23b8b658b81823d
Romeo Bellon <romeo.bellon@me.com>
2015-12-02 19:46:14 -0500

    Some issues are fixed now. Devices listed on the left.

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -3,34 +3,55 @@ package main;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
 import org.jnetpcap.Pcap;
 import org.jnetpcap.PcapIf;
 
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ResourceBundle;
 
-public class Controller {
-    List<PcapIf> alldevs = new ArrayList<>(); // Will be filled with NICs
-    StringBuilder errbuf = new StringBuilder(); // For any error msgs
-    ObservableList<String> devices = FXCollections.observableArrayList();
+public class Controller implements Initializable {
+    List<PcapIf> alldevs = new ArrayList<>(); // Filled with devices
+    StringBuilder errbuf = new StringBuilder(); // Error messages
+    ObservableList<String> devices = FXCollections.observableArrayList(); // Dynamic list of devices
 
     @FXML
-    TextField textfield_ip;
+    Label selected_device;
     @FXML
-    ListView<String> list_devices = new ListView<>(devices);
+    ListView<String> list_devices;
 
     public void getDevices() {
+        devices.removeAll();
         int r = Pcap.findAllDevs(alldevs, errbuf);
         if (r == Pcap.NOT_OK || alldevs.isEmpty()) {
             System.err.printf("Can't read list of devices, error is %s", errbuf.toString());
         } else {
-            System.out.println(r);
-            devices.removeAll();
             for (PcapIf alldev : alldevs) {
                 devices.add(alldev.getName());
             }
         }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // Set content to ListView from the beginning
+        list_devices.setItems(devices);
+
+        // Show devices from the beginning
+        this.getDevices();
+
+        // Selection listener
+        list_devices.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+                    selected_device.setText(newValue);
+                }
+        );
+
+        // Automatically select eth0
+        if (devices.contains("eth0")) list_devices.getSelectionModel().select("eth0");
+
     }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -8,16 +8,16 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
-    }
-
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        primaryStage.setTitle("TracerouteApplication");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
     }
 }
